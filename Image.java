d1404d06960c81fcf9b0b7e914bdfe31f44bee98
Kévin Froissart <kevin.froissart69@gmail.com>
2021-09-07 14:51:12 +0200

    TP1 terminé

---
+++ Image.java
@@ -0,0 +1,42 @@
+public class Image {
+    private int hauteur;
+    private int largeur;
+    private Pixel tabPixel[][];
+
+    public Image(int hauteur, int largeur){
+        this.hauteur = hauteur;
+        this.largeur = largeur;
+        this.tabPixel = new Pixel[hauteur][largeur];
+        for(int h = 0; h < hauteur; h++){
+            for(int l = 0; l < largeur; l++){
+                this.tabPixel[h][l] = new Pixel();
+            }
+        }
+    }
+
+    public Pixel getPixel(int hauteur, int largeur){
+        return this.tabPixel[hauteur][largeur];
+    }
+
+    public void setPixel(int hauteur, int largeur, Pixel pixel){
+        this.tabPixel[hauteur][largeur] = pixel;
+    }
+
+    public String toString(){
+        String res = "";
+        for(int h = 0; h < this.hauteur; h++){
+            res+= "[";
+            for(int l = 0; l < this.largeur; l++){
+                res += this.tabPixel[h][l];
+                if(l+1 != largeur) res+="|";
+            }
+            res+= "]\n";
+        }
+        return res;
+    }
+
+    public static void main(String args[]){
+        Image i = new Image(10,10);
+        System.out.println(i.toString());
+    }
+}
--- Pixel.cpp
+++ Pixel.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+
 
 class Pixel
 {
@@ -10,10 +12,11 @@ public :
     void setUn(int);
     void setDeux(int);
     void setTrois(int);
+    void print();
 private:
-int un;
-int deux;
-int trois;
+    int un;
+    int deux;
+    int trois;
 };
 
 int Pixel::getUn(){
@@ -40,14 +43,54 @@ void Pixel::setTrois(int trois){
     this->trois = trois;
 }
 
+void Pixel::print(){
+    std::cout << this->un << "." << this->deux << "." << this->trois;
+}
+
 class Image
 {
 public :
-    Image(int hauteur,const int largeur){
-        pixel = new int[hauteur][largeur];
+    Image(int hauteur, int largeur){
+        this->tabPixel.resize(hauteur);
+        for(int h = 0; h < hauteur; h++){
+            this->tabPixel[h].resize(largeur);
+            for(int l = 0; l < largeur; l++){
+                this->tabPixel[h][l] = new Pixel();
+            }
+        }
+        this->hauteur = hauteur;
+        this->largeur = largeur;
     }
+    void setPixel(int, int, Pixel*);
+    Pixel* getPixel(int, int);
+    void print();
 private:
-int hauteur;
-int largeur;
-int **pixel;
+    int hauteur;
+    int largeur;
+    std::vector<std::vector<Pixel*> > tabPixel;
 };
+
+void Image::setPixel(int hauteur, int largeur, Pixel* pixel) {
+    this->tabPixel[hauteur][largeur] = pixel;
+}
+
+Pixel* Image::getPixel(int hauteur, int largeur){
+    return this->tabPixel[hauteur][largeur];
+}
+
+void Image::print(){
+    for(int h = 0; h < this->hauteur; h++){
+        std::cout << "[";
+        for(int l = 0; l < this->largeur; l++){
+            this->getPixel(h,l)->print();
+            if(l+1 != largeur) std::cout << "|";
+        }
+        std::cout << "]" << std::endl;
+    }
+}
+
+int main(){
+    Image* i = new Image(10,10);
+    i->print();
+    return 0;
+}
