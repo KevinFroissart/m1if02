4b293c77553417c0d309d36819e7277c4f62cae4
KevinFroissart <kevin.froissart69@gmail.com>
2021-09-07 22:25:58 +0200

    exo 3 TP 2

--- TP1/LaClasse.cpp
+++ TP1/LaClasse.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
-
 /*
 *
 * Ligne de commande pour compiler : g++ -std=c++14 -fno-elide-constructors Laclasse.cpp
@@ -10,25 +9,41 @@
 
 class LaClasse
 {
-public :
+public:
   //Construction, conversion, affectation et destruction
   LaClasse() : l(0)
-  {std::cout << "LaClasse::LaClasse()\n";}
-  LaClasse(const LaClasse & lc) : l(lc.l)
-  {std::cout << "LaClasse::LaClasse(const LaClasse & )\n";}
-  LaClasse(int i) : l(i) 
-  {std::cout << "LaClasse::LaClasse(int)\n";}
+  {
+    std::cout << "LaClasse::LaClasse()\n";
+  }
+  LaClasse(const LaClasse &lc) : l(lc.l)
+  {
+    std::cout << "LaClasse::LaClasse(const LaClasse & )\n";
+  }
+  LaClasse(int i) : l(i)
+  {
+    std::cout << "LaClasse::LaClasse(int)\n";
+  }
   operator bool() const
-  {std::cout << "LaClasse::operator bool() const\n"; return true;}
+  {
+    std::cout << "LaClasse::operator bool() const\n";
+    return true;
+  }
   ~LaClasse()
-  {std::cout << "~LaClasse::LaClasse()\n";}
-  const LaClasse & operator=(const LaClasse & c)
-  {l=c.l; std::cout << "LaClasse::operator=(const LaClasse &)\n"; return *this;}
+  {
+    std::cout << "~LaClasse::LaClasse()\n";
+  }
+  const LaClasse &operator=(const LaClasse &c)
+  {
+    l = c.l;
+    std::cout << "LaClasse::operator=(const LaClasse &)\n";
+    return *this;
+  }
   //Autre fonction membre
   LaClasse F(LaClasse);
   // Déclaration fonction extérieure amie
-  friend std::ostream & operator << (std::ostream & os, const LaClasse & lc);
-private :
+  friend std::ostream &operator<<(std::ostream &os, const LaClasse &lc);
+
+private:
   int l;
 };
 
@@ -44,44 +59,45 @@ LaClasse LaClasse::F(LaClasse v)
   return ::F(v);
 }
 
-std::ostream & operator << (std::ostream & os, const LaClasse & lc)
+std::ostream &operator<<(std::ostream &os, const LaClasse &lc)
 {
-  os << " in ostream << LaClasse "<< lc.l << std::endl;
+  os << " in ostream << LaClasse " << lc.l << std::endl;
   return os;
 }
 
-// Testez et analysez la séquence d'appels aux fonctions membres 
+// Testez et analysez la séquence d'appels aux fonctions membres
 // de LaClasse dans le programme suivant :
-
 
 int main()
 {
-  LaClasse c1; //copy
-  LaClasse c2=LaClasse(); //copy
-  LaClasse cc1(c1); 
-  LaClasse cc2=c1; //copy
-  LaClasse cc3=LaClasse(c1); // copy + copy
-  LaClasse cv1(5); // int
-  LaClasse cv2=5; // int
-  LaClasse cv3=LaClasse(5); // int + copy 
-  LaClasse cv4=(LaClasse)5; // int + copy
+  LaClasse c1;              //copy
+  LaClasse c2 = LaClasse(); //copy
+  LaClasse cc1(c1);
+  LaClasse cc2 = c1;           //copy
+  LaClasse cc3 = LaClasse(c1); // copy + copy
+  LaClasse cv1(5);             // int
+  LaClasse cv2 = 5;            // int
+  LaClasse cv3 = LaClasse(5);  // int + copy
+  LaClasse cv4 = (LaClasse)5;  // int + copy
   std::cout << std::endl;
-  c1=cc1; // opérateur = (d'affectation) (si on met le type au début : pas une affectation mais déclarations)
+  c1 = cc1; // opérateur = (d'affectation) (si on met le type au début : pas une affectation mais déclarations)
   std::cout << std::endl;
-  c2=8; // int + opérateur =
-  if(LaClasse()){} // default + bool + ~
+  c2 = 8; // int + opérateur =
+  if (LaClasse())
+  {
+  } // default + bool + ~
   std::cout << std::endl;
-  if(cv1) // opérateur bool
-    {
-      cv1=F(10); // int + copy + int + copy + opérateur = (premier copy car le param est passé en copy, second copy car return passé en copy)
-      cv1=F(c1); // copy + int + copy + opérateur =
-      cv1=c1.F(c2); // copy c2 + copy vv + int + copy + copy + opérateur = (pas de copy car l'opé = prends en paramètre une réfeérence constance ce qui évite de faire une copie)
-    }
+  if (cv1) // opérateur bool
+  {
+    cv1 = F(10);    // int + copy + int + copy + opérateur = (premier copy car le param est passé en copy, second copy car return passé en copy)
+    cv1 = F(c1);    // copy + int + copy + opérateur =
+    cv1 = c1.F(c2); // copy c2 + copy vv + int + copy + copy + opérateur = (pas de copy car l'opé = prends en paramètre une réfeérence constance ce qui évite de faire une copie)
+  }
 
   std::cout << "Tableaux \n";
   LaClasse tab[3];
-  LaClasse *pc=new LaClasse(tab[0]);
+  LaClasse *pc = new LaClasse(tab[0]);
   delete pc;
   std::cout << "Avant de sortir ... \n";
-  return 0; 
+  return 0;
 }
--- TP2/LaClasse.cpp
+++ TP2/LaClasse.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
-
 /*
 *
 * Ligne de commande pour compiler : g++ -std=c++14 -fno-elide-constructors Laclasse.cpp
@@ -10,14 +9,14 @@
 
 class LaClasse
 {
-public :
+public:
   //Construction, conversion, affectation et destruction
   LaClasse() : l(0), ptr(new int(0))
   {
     std::cout << "LaClasse::LaClasse()\n";
   }
 
-  LaClasse(const LaClasse & lc) : l(lc.l), ptr(new int(*lc.ptr))
+  LaClasse(const LaClasse &lc) : l(lc.l), ptr(new int(*lc.ptr))
   {
     std::cout << "LaClasse::LaClasse(const LaClasse & )\n";
   }
@@ -27,41 +26,47 @@ public :
     std::cout << "LaClasse::LaClasse(int)\n";
   }
 
-  LaClasse(LaClasse&& other) : l(other.l), ptr(other.ptr){
+  LaClasse(LaClasse &&other) : l(other.l), ptr(other.ptr)
+  {
     other.ptr = nullptr;
   }
 
   operator bool() const
-  {std::cout << "LaClasse::operator bool() const\n"; return true;}
+  {
+    std::cout << "LaClasse::operator bool() const\n";
+    return true;
+  }
 
   ~LaClasse()
   {
     std::cout << "~LaClasse::LaClasse()\n";
-    delete ptr;  
+    delete ptr;
   }
 
-  const LaClasse & operator=(const LaClasse & c)
+  const LaClasse &operator=(const LaClasse &c)
   {
-    l=c.l; 
-    ptr=new int(*c.ptr); 
-    std::cout << "LaClasse::operator=(const LaClasse &)\n"; 
+    l = c.l;
+    ptr = new int(*c.ptr);
+    std::cout << "LaClasse::operator=(const LaClasse &)\n";
     return *this;
   }
 
-  LaClasse& operator=(LaClasse&& m){
+  LaClasse &operator=(LaClasse &&m)
+  {
     l = m.l;
     delete ptr;
     ptr = m.ptr;
     m.ptr = nullptr;
-    return *this; 
+    return *this;
   }
 
   //Autre fonction membre
   LaClasse F(LaClasse);
   // Déclaration fonction extérieure amie
-  friend std::ostream & operator << (std::ostream & os, const LaClasse & lc);
-private :
-  int * ptr;
+  friend std::ostream &operator<<(std::ostream &os, const LaClasse &lc);
+
+private:
+  int *ptr;
   int l;
 };
 
@@ -77,44 +82,82 @@ LaClasse LaClasse::F(LaClasse v)
   return ::F(v);
 }
 
-std::ostream & operator << (std::ostream & os, const LaClasse & lc)
+std::ostream &operator<<(std::ostream &os, const LaClasse &lc)
 {
-  os << " in ostream << LaClasse "<< lc.l << std::endl;
+  os << " in ostream << LaClasse " << lc.l << std::endl;
   return os;
 }
 
-// Testez et analysez la séquence d'appels aux fonctions membres 
+// Testez et analysez la séquence d'appels aux fonctions membres
 // de LaClasse dans le programme suivant :
 
+class LaClasseSpecialisee : public LaClasse
+{
+public:
+  LaClasseSpecialisee() : t(0)
+  {
+    std::cout << "LaClasseSpecialisee::LaClasseSpecialisee()" << std::endl;
+  }
+
+  LaClasseSpecialisee(const LaClasseSpecialisee &lc) : t(lc.t)
+  {
+    std::cout << "LaClasseSpecialisee::LaClasseSpecialisee(const LaClasseSpecialisee & )\n";
+  }
+
+  LaClasseSpecialisee &operator=(LaClasseSpecialisee &&m)
+  {
+    t = m.t;
+    std::cout << "LaClasseSpecialisee::operator=(const LaClasseSpecialisee &)" << std::endl;
+    return *this;
+  }
+
+  ~LaClasseSpecialisee() {}
+
+private:
+  int t;
+};
 
 int main()
 {
-  LaClasse c1; //copy
-  LaClasse c2=LaClasse(); //copy
-  LaClasse cc1(c1); 
-  LaClasse cc2=c1; //copy
-  LaClasse cc3=LaClasse(c1); // copy + copy
-  LaClasse cv1(5); // int
-  LaClasse cv2=5; // int
-  LaClasse cv3=LaClasse(5); // int + copy 
-  LaClasse cv4=(LaClasse)5; // int + copy
+  LaClasse c1;                 //copy
+  LaClasse c2 = LaClasse();    //copy
+  LaClasse cc1(c1);            //copy
+  LaClasse cc2 = c1;           //copy
+  LaClasse cc3 = LaClasse(c1); // copy + copy
+  LaClasse cv1(5);             // int
+  LaClasse cv2 = 5;            // int
+  LaClasse cv3 = LaClasse(5);  // int + copy
+  LaClasse cv4 = (LaClasse)5;  // int + copy
   std::cout << std::endl;
-  c1=cc1; // opérateur = (d'affectation) (si on met le type au début : pas une affectation mais déclarations)
+  c1 = cc1; // opérateur = (d'affectation) (si on met le type au début : pas une affectation mais déclarations)
   std::cout << std::endl;
-  c2=8; // int + opérateur =
-  if(LaClasse()){} // default + bool + ~
+  c2 = 8; // int + opérateur =
+  if (LaClasse())
+  {
+  } // default + bool + ~
   std::cout << std::endl;
-  if(cv1) // opérateur bool
-    {
-      cv1=F(10); // int + copy + int + copy + opérateur = (premier copy car le param est passé en copy, second copy car return passé en copy)
-      cv1=F(c1); // copy + int + copy + opérateur =
-      cv1=c1.F(c2); // copy c2 + copy vv + int + copy + copy + opérateur = (pas de copy car l'opé = prends en paramètre une réfeérence constance ce qui évite de faire une copie)
-    }
+  if (cv1) // opérateur bool
+  {
+    cv1 = F(10);    // int + copy + int + copy + opérateur = (premier copy car le param est passé en copy, second copy car return passé en copy)
+    cv1 = F(c1);    // copy + int + copy + opérateur =
+    cv1 = c1.F(c2); // copy c2 + copy vv + int + copy + copy + opérateur = (pas de copy car l'opé = prends en paramètre une réfeérence constance ce qui évite de faire une copie)
+  }
 
   std::cout << "Tableaux \n";
   LaClasse tab[3];
-  LaClasse *pc=new LaClasse(tab[0]);
+  LaClasse *pc = new LaClasse(tab[0]);
   delete pc;
-  std::cout << "Avant de sortir ... \n";
-  return 0; 
+
+  std::cout << "\nDérivée\n";
+  LaClasseSpecialisee lc1;
+  LaClasseSpecialisee lc2 = LaClasseSpecialisee();
+
+  std::cout << "\nUpcast\n";
+  LaClasse uc1 = lc1;
+
+  //std::cout << "\nDowncast\n";
+  //LaClasseSpecialisee dc1 = c1;
+
+  std::cout << "\nAvant de sortir ... \n";
+  return 0;
 }
