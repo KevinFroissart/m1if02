1a8ddcacdf381dbdeb2eb91c60803a2f823f1901
KevinFroissart <kevin.froissart69@gmail.com>
2021-09-08 22:00:34 +0200

    exo 3 TP 2

--- TP2/String.cpp
+++ TP2/String.cpp
@@ -4,28 +4,64 @@
 class String
 {
 public:
-    String() : string(nullptr)
+    String() : string(nullptr), len(0)
     {
         std::cout << "String::String()" << std::endl;
     }
-    String(char c) : string(new char(c))
+    String(char c) : string(new char(c)), len(1)
     {
         std::cout << "String::String(char)" << std::endl;
     }
     String(const char* c)
     {
-        const size_t len = strlen(c);
-        char* tmp_c = new char[len + 1];
-        strncpy(tmp_c, c, len);
+        len = strlen(c);
+        const size_t length = strlen(c);
+        char* tmp_c = new char[length + 1];
+        strncpy(tmp_c, c, length);
         string = tmp_c;
         std::cout << "String::String(const char*)" << std::endl;
     }
+
+    const String operator+(const String &c) 
+    {    
+        String s;
+        len = len + c.len;
+        s.string = new char[len + 1];
+        strcpy(s.string, string);
+        strcpy(s.string, c.string);
+        return s;
+    }
+
     const String &operator=(const String &s)
     {
-        string = new char(s.string);
+        if(this!=&s)
+        {
+            if(string) delete string;
+            string = new char[s.len + 1];
+            strcpy(string, s.string);
+        }
+        return *this;
     }
+
+    /*const String &operator=(String &&s)
+    {
+        delete string;
+        string = s.string;
+        s.string = nullptr;
+        return *this;
+    }*/
+
+    /*
+    LaClasse &operator=(LaClasse &&m)
+  {
+    l = m.l;
+    delete ptr;
+    ptr = m.ptr;
+    m.ptr = nullptr;
+    return *this;
+  }*/
+    
     /*const String &operator=(const String &&s){}
-    const String operator+(const String s){}
     const bool operator==(const String s){}
     const bool operator!=(const String s){}
     const bool operator<(const String s){}
@@ -39,6 +75,7 @@ public:
     }
 private:
     char *string;
+    int len;
 };      
 
 int main()
@@ -48,4 +85,8 @@ int main()
     String s3("a");
     String s4("abc");
     String s5("#]@");
+    String s6 = s3 + s4;
+    String s7 = s4;
+
+    return 0;
 }
